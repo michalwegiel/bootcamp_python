e64df55130e4ca884c910968fb3f8662b60f82cf
Michal Wegiel <michal.wegiel@aptiv.com>
2022-04-01 12:17:12 +0200

    Prime numbers generator added

--- prime_factors/prime_factors.py
+++ prime_factors/prime_factors.py
@@ -1,5 +1,13 @@
-def prime_generator():
-    pass
+def prime_generator(n):
+    def is_prime(number_to_check):
+        for i in range(2, number_to_check - 1):
+            if number_to_check % i == 0:
+                return False
+        return True
+
+    for number in range(1, n + 1):
+        if is_prime(number):
+            yield number
 
 
 def prime_factors(number):
--- prime_factors/test_prime_factors.py
+++ prime_factors/test_prime_factors.py
@@ -5,7 +5,11 @@ from prime_factors import prime_generator, prime_factors
 class TestPrimeGenerator(unittest.TestCase):
 
     def test_prime_generator(self):
-        assert True
+        prime_numbers = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23]
+        outputs = [[i for i in prime_generator(23)], [i for i in prime_generator(26)]]
+        assert prime_numbers == outputs[0]
+        assert prime_numbers == outputs[1]
+
 
 
 class TestPrimeFactors(unittest.TestCase):
