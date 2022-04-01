abc237a16ae7444520bfb20a3baac89aec583b7d
Michal Wegiel <michal.wegiel@aptiv.com>
2022-04-01 11:36:21 +0200

    Add tests template

---
+++ prime_factors/test_prime_factors.py
@@ -0,0 +1,18 @@
+import unittest
+from prime_factors import prime_generator, prime_factors
+
+
+class TestPrimeGenerator(unittest.TestCase):
+
+    def test_prime_generator(self):
+        assert True
+
+
+class TestPrimeFactors(unittest.TestCase):
+
+    def test_prime_factors(self):
+        assert True
+
+
+if __name__ == '__main__':
+    unittest.main()
