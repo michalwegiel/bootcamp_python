09bb7f51e86a105db254b44a20e8f781dfad46f9
Michal Wegiel <michal.wegiel@aptiv.com>
2022-04-04 20:42:32 +0200

    docstring tests added

--- systemy_liczbowe/roman_to_int.py
+++ systemy_liczbowe/roman_to_int.py
@@ -1,2 +1,13 @@
-def roman_to_int(roman):
+def roman_to_int(roman: str) -> int:
+    """
+    :param roman: str
+    :return: int
+    Tests:
+    >>> roman_to_int("I")
+        1
+    >>> roman_to_int("XI")
+        11
+    >>> roman_to_int("L")
+        50
+    """
     return 0
