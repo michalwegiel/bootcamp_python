23750bbe275322ba9c0d4081a6ffa69d532627c8
Michal Wegiel <michal.wegiel@aptiv.com>
2022-04-04 21:05:15 +0200

    implementation of roman_to_int function

--- systemy_liczbowe/roman_to_int.py
+++ systemy_liczbowe/roman_to_int.py
@@ -1,13 +1,36 @@
+from typing import Dict
+
+
 def roman_to_int(roman: str) -> int:
     """
     :param roman: str
     :return: int
     Tests:
     >>> roman_to_int("I")
-        1
+    1
     >>> roman_to_int("XI")
-        11
+    11
     >>> roman_to_int("L")
-        50
+    50
     """
-    return 0
+    symbols: Dict[str, int] = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+    value: int = 0
+    previous: int = 0
+
+    for i in roman:
+        current = symbols[i]
+        if current > previous:
+            value = value + current - 2 * previous
+        else:
+            value += current
+        previous = current
+
+    return value
