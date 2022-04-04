992227ca07beec9ab6e977fb0c51ea679f984204
Michal Wegiel <michal.wegiel@aptiv.com>
2022-04-04 21:15:58 +0200

    Negative scenarios added

--- systemy_liczbowe/test_roman_to_int.py
+++ systemy_liczbowe/test_roman_to_int.py
@@ -36,3 +36,17 @@ def test_roman_to_int_advanced_scenarios():
     }
     for k, v in scenarios.items():
         assert roman_to_int(k) == v
+
+
+def test_roman_to_negative():
+    scenarios = {
+        1: ValueError,
+        2.4: ValueError,
+        [1, 2]: ValueError,
+        {"I": 1}: ValueError,
+        None: ValueError,
+        "": ValueError,
+    }
+    for k, v in scenarios.items():
+        with pytest.raises:
+            assert roman_to_int(k) == v
