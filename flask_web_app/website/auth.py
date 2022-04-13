5c0d6958db703aab873e02b7b93b208063ae4cdc
Michal Wegiel <michal.wegiel@aptiv.com>
2022-04-13 10:26:45 +0200

    test and implementation for empty signup form

--- flask_web_app/website/auth.py
+++ flask_web_app/website/auth.py
@@ -49,28 +49,31 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        user = User.query.filter_by(email=email).first()
-        if user:
-            flash('Email already exists.', category='error')
-        elif len(email) < 4:
-            flash('Not valid email.', category='error')
-        elif len(first_name) < 2:
-            flash('First name must be greater than 1 character.', category='error')
-        elif password1 != password2:
-            flash('Passwords do not match.', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
+        if email == "" or first_name == "" or github == "" or password1 == "" or password2 == "":
+            flash('Fill in form!', category='error')
         else:
-            new_user = User(
-                email=email,
-                first_name=first_name,
-                github=github,
-                password=generate_password_hash(password1, method='sha256'),
-            )
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Account created!', category='success')
-            login_user(new_user, remember=True)
-            return redirect(url_for('views.home'))
+            user = User.query.filter_by(email=email).first()
+            if user:
+                flash('Email already exists.', category='error')
+            elif len(email) < 4:
+                flash('Not valid email.', category='error')
+            elif len(first_name) < 2:
+                flash('First name must be greater than 1 character.', category='error')
+            elif password1 != password2:
+                flash('Passwords do not match.', category='error')
+            elif len(password1) < 7:
+                flash('Password must be at least 7 characters.', category='error')
+            else:
+                new_user = User(
+                    email=email,
+                    first_name=first_name,
+                    github=github,
+                    password=generate_password_hash(password1, method='sha256'),
+                )
+                db.session.add(new_user)
+                db.session.commit()
+                flash('Account created!', category='success')
+                login_user(new_user, remember=True)
+                return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
